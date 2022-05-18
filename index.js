a06bd6890118fe364e1297888698c6344d5ea46f
cckira <88904909+cckira@users.noreply.github.com>
2022-05-18 14:46:27 -0400

    Changing the side of the icon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var randomNumber2 = Math.floor((Math.random())*6+1);
 document.querySelector("img").setAttribute("src","images/dice"+randomNumber1+".png");
 document.querySelectorAll("img")[1].setAttribute("src","images/dice"+randomNumber2+".png");
 if (randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML = "Player 1 Wins! 🎉"
+  document.querySelector("h1").innerHTML = "🎉 Player 1 Wins!"
 }
 else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🎉"
